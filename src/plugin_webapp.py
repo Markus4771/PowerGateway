b595dc35fe78b224ae28a372327507e35a1bf5b4
Markus4771 <162835583+Markus4771@users.noreply.github.com>
2026-07-29 12:49:52 +0200

    Binde Export-Center in PowerGateway-Weboberfläche ein

--- src/plugin_webapp.py
+++ src/plugin_webapp.py
@@ -10,6 +10,7 @@ import letsencrypt_web
 import dashboard_runtime
 import energy_history
 import energy_history_professional
+import export_center
 import network_diagnostics_runtime
 import gateway_selector
 import internet_manager
